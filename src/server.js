c9a76137c02b733931bdce179f732959f671565b
kaf212 <kaf221122@gmail.com>
2025-05-07 14:27:09 +0200

    switch to mongoose in db.js

--- src/server.js
+++ src/server.js
@@ -6,7 +6,11 @@ const connectDB = require('./config/db'); // adjust the path if needed
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+app.use(express.json());
 
+app.use('/api/orders', require('./routes/orderRoutes'));
+app.use('/api/products', require('./routes/productRoutes'));
+app.use('/api/users', require('./routes/userRoutes'));
 
 (async () => {
     const db = await connectDB();
