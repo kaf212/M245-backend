c71833d4d4555a7ab72f2b3831d933bbf309bb82
kaf212 <kaf221122@gmail.com>
2025-05-07 15:53:11 +0200

    implemented authentication and authorization middleware

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const User = require('../models/User');
+const bcrypt = require("bcryptjs")
+const {authorizeStandard, authorizeAdmin} = require("../middleware/authorization")
+const jwt = require("jsonwebtoken");
 
 // Get all users
 router.get('/', async (req, res) => {
@@ -16,16 +19,41 @@ router.get('/:userId', async (req, res) => {
 
 // Register new user
 router.post('/register', async (req, res) => {
-    const newUser = new User(req.body);
-    const saved = await newUser.save();
-    res.status(201).json(saved);
+    try {
+        const { password, ...rest } = req.body;
+
+        const hashedPassword = await bcrypt.hash(password, 10);
+
+        const newUser = new User({
+            ...rest,
+            password: hashedPassword
+        });
+
+        const saved = await newUser.save();
+        res.status(201).json(saved);
+    } catch (err) {
+        res.status(400).json({ error: err.message });
+    }
 });
 
 // Login (mock - replace with real auth)
 router.post('/login', async (req, res) => {
-    const user = await User.findOne({ email: req.body.email });
-    if (!user || user.password !== req.body.password) return res.status(401).json({ error: 'Invalid credentials' });
-    res.json(user);
+    const { email, password } = req.body;
+    console.log(email, password)
+    const user = await User.findOne({ email });
+    if (!user || !(await bcrypt.compare(password, user.password))) {
+        return res.status(401).json({ error: 'Invalid credentials' });
+    }
+
+    const userRole = (User.isAdmin) ? "admin" : "standard"
+
+    const token = jwt.sign(
+        { id: user._id, email: user.email, role: userRole },
+        process.env.SECRET_KEY,
+        { expiresIn: '1h' }
+    );
+
+    res.json({ token });
 });
 
 // Update user
