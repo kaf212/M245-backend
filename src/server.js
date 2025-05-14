9c323aedc84578faf6161836d3664e610a797c5c
kaf212 <kaf221122@gmail.com>
2025-05-14 08:56:22 +0200

    implemented cors

--- src/server.js
+++ src/server.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const path = require('path');
 require('dotenv').config({ path: path.join(__dirname, '../.env') });
+const cors = require("cors")
 const connectDB = require('./config/db'); // adjust the path if needed
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+app.use(cors())
 app.use(express.json());
 
 app.use('/api/orders', require('./routes/orderRoutes'));
